a890635c27c0086680c467b8e5e9a1ed93a361ad
Rovshen Berdimyradov <rovshen.berdimyradov@emergn.com>
2025-09-04 23:25:29 +0500

    add background to service details

--- apps/services/serializers.py
+++ apps/services/serializers.py
@@ -89,7 +89,7 @@ class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = [
-            'id', 'vendor', 'category', 'avatar',
+            'id', 'vendor', 'category', 'avatar', 'background',
             'title_tm', 'title_ru', 'title_en',
             'description_tm', 'description_ru', 'description_en',
             'price_min', 'price_max', 'is_catalog',
